c898a9064518d8ca9537698b00cb44033efa8d88
YuanChi Hsieh <60728633+boring89@users.noreply.github.com>
2026-01-08 09:38:36 +0800

    Add Mechanism2d

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -7,10 +10,22 @@ public class ShooterSubsystem extends SubsystemBase {
     
     private final ShooterIO io;
 
+    private final Mechanism2d shooterMechanism;
+
+    private final MechanismRoot2d shooterRoot;
+
+    private final MechanismLigament2d shooterAngle;
+
     private double targetSpeed = 0.0, targetAngle = 0.0;
 
     public ShooterSubsystem(ShooterIO io) {
         this.io = io;
+
+        this.shooterMechanism = new Mechanism2d(1.0, 1.0);
+        this.shooterRoot = shooterMechanism.getRoot("Wrist", 0.0, 0.0);
+        this.shooterAngle = shooterRoot.append(
+                new MechanismLigament2d("Wrist Angle", 1.0, 0.0));
+
         SmartDashboard.putNumber("Shooter/targetSpeed", targetSpeed);
         SmartDashboard.putNumber("shooter/targetAngle", targetAngle);
     }
@@ -18,17 +33,24 @@ public class ShooterSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
-        DashboardControl();
+        Dashboard();
 
         io.setShooterSpeed(targetSpeed);
         io.setShooterAngle(targetAngle);
         
     }
 
-    public void DashboardControl() {
-        SmartDashboard.getNumber("targetAngle", 0.0);
-        SmartDashboard.getNumber("targetSpeed", 0.0);
+    public void Dashboard() {
+        SmartDashboard.getNumber("Shooter/targetAngle", 0.0);
+        SmartDashboard.getNumber("Shooter/targetSpeed", 0.0);
 
+        SmartDashboard.putNumber("Shooter/currentSpeed", this.io.getShooterSpeed());
+        SmartDashboard.putNumber("Shooter/currentAngle", this.io.getShooterAngle());
+        SmartDashboard.putData("Shooter/Wrist", shooterMechanism);  
+    }
+
+    public void setMechanism2d() {
+        shooterAngle.setAngle(this.io.getShooterAngle());
     }
 
 }
